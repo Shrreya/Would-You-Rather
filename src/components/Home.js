28294a2a5363c8ea00d92a116ea494d306787482
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-13 21:20:05 +0530

    Fetch initial data on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 import Nav from './Nav';
 
 class Home extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData(this.props.location.state.user_id));
+  }
+
   render() {
     return (
       <div>
         <Nav />
-        <span>Home Page</span>
+        <ul>
+          {this.props.questionIds.map((id) => (
+            <li key={id}>
+              <div>QUESTION ID: {id}</div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
 }
 
-export default Home;
+function mapStateToProps({ questions }) {
+  return {
+    questionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Home);
