340ea954d5ea98ffc15870334d2a9a04a28c73cc
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-18 17:52:55 +0530

    Added click handler & dispatch for answers

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Nav from './Nav';
 import { Line } from 'rc-progress';
+import { handleAnswer } from '../actions/shared';
 
 class QuestionPage extends Component {
 
+  handleClick = (answer) => {
+    const { dispatch, qid } = this.props;
+    dispatch(handleAnswer(qid, answer));
+  }
+
   render() {
 
-    const { id, hasAnswered, authorName, authorAvatar, optionOne, optionTwo,
-      optionChosen, optionOneVotes, optionTwoVotes } = this.props;
+    const { hasAnswered, authorName, authorAvatar, optionOne, optionTwo,
+      answer, optionOneVotes, optionTwoVotes } = this.props;
 
     const totalVotes = optionOneVotes + optionTwoVotes;
     const optionOnePerc = ((optionOneVotes / totalVotes ) * 100).toString();
@@ -21,7 +27,7 @@ class QuestionPage extends Component {
         <span className='author-prompt'>{authorName} asks</span>
         <h2>Would you rather ...</h2>
         {
-          hasAnswered && optionChosen === 'optionOne' && <div>
+          hasAnswered && answer === 'optionOne' && <div>
             <div className='option-close option-chosen'>
               <div className='text-chosen'><strong>{optionOne}?</strong></div>
               <Line percent={optionOnePerc} strokeColor='#00897B' className='percentage'/>
@@ -37,7 +43,7 @@ class QuestionPage extends Component {
           </div>
         }
         {
-          hasAnswered && optionChosen === 'optionTwo' && <div>
+          hasAnswered && answer === 'optionTwo' && <div>
             <div className='option-close'>
               <div><strong>{optionOne}?</strong></div>
               <Line percent={optionOnePerc} strokeColor='#00897B' className='percentage'/>
@@ -54,10 +60,10 @@ class QuestionPage extends Component {
         }
         {
           !(hasAnswered) && <div>
-            <div className='option-open'>
+            <div className='option-open' onClick={() => this.handleClick('optionOne')}>
               {optionOne}?
             </div>
-            <div className='option-open'>
+            <div className='option-open' onClick={() => this.handleClick('optionTwo')}>
               {optionTwo}?
             </div>
           </div>
@@ -71,15 +77,15 @@ function mapStateToProps({authedUser, questions, users}, props) {
   const { id } = props.match.params;
   const question = questions[id];
   const hasAnswered = Object.keys(users[authedUser]['answers']).includes(id);
-  const optionChosen = hasAnswered ? users[authedUser]['answers'][id] : '';
+  const answer = hasAnswered ? users[authedUser]['answers'][id] : '';
   return {
-    id,
+    qid: id,
     hasAnswered,
     authorName: users[question['author']]['name'],
     authorAvatar: users[question['author']]['avatarURL'],
     optionOne: question['optionOne']['text'],
     optionTwo: question['optionTwo']['text'],
-    optionChosen,
+    answer,
     optionOneVotes: question['optionOne']['votes'].length,
     optionTwoVotes: question['optionTwo']['votes'].length
   }
