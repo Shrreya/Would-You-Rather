600d80e60b7c63c6287261308ed954b2a8d79737
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-18 17:42:07 +0530

    Added action type to save answer for question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,18 @@
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
+export const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER';
 
 export function receiveQuestions (questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions
+  }
+}
+
+export function questionAnswer (info) {
+  return {
+    type: SAVE_QUESTION_ANSWER,
+    authedUser: info.authedUser,
+    qid: info.qid,
+    answer: info.answer
   }
 }
