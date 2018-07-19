a2b41e3afe09b15565d5ae68a72cfd794c8ef1e3
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-19 16:14:25 +0530

    Removed authed user dispatch from initial data load

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,20 +1,18 @@
 import { getInitialData } from '../utils/api';
 import { receiveUsers } from '../actions/users';
 import { receiveQuestions } from '../actions/questions';
-import { setAuthedUser } from '../actions/authedUser';
 import { showLoading, hideLoading } from 'react-redux-loading';
 import { saveQuestionAnswer } from '../utils/api';
 import { questionAnswer } from '../actions/questions';
 import { userAnswer } from '../actions/users';
 
-export function handleInitialData (authed_id) {
+export function handleInitialData () {
   return (dispatch) => {
     dispatch(showLoading());
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users));
         dispatch(receiveQuestions(questions));
-        dispatch(setAuthedUser(authed_id));
         dispatch(hideLoading());
       })
   }
