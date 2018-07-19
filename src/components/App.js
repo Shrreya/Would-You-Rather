fb77b1a694c7893bb81c0a9f3235b339872be45c
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-19 16:22:08 +0530

    Load initial data in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoadingBar from 'react-redux-loading';
 import Login from './Login';
 import Home from './Home';
 import QuestionPage from './QuestionPage';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <Router>
-        <div className='app'>
-          <Route exact path='/' component={Login} />
-          <Route exact path='/home' component={Home} />
-          <Route path='/questions/:id' component={QuestionPage} />
-        </div>
+        {
+          this.props.loading
+            ? <LoadingBar style={{ backgroundColor: '#00897B'}}/>
+            : <div className='app'>
+              <Route exact path='/' component={Login} />
+              <Route exact path='/home' component={Home} />
+              <Route path='/questions/:id' component={QuestionPage} />
+            </div>
+        }
       </Router>
     );
   }
 }
 
-export default App;
+function mapStateToProps({ questions }) {
+  return {
+    loading: questions === {}
+  }
+}
+
+export default connect(mapStateToProps)(App);
