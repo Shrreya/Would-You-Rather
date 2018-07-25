cf38625dc33ac9117daa367e13f7a4bb6c909f72
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-25 19:50:19 +0530

    Fixed Loading Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -18,17 +18,20 @@ class App extends Component {
   render() {
     return (
       <Router>
-        {
-          this.props.loading
-            ? <LoadingBar style={{ backgroundColor: '#00897B'}}/>
-            : <div className='app'>
-              <Route exact path='/' component={Login} />
-              <Route exact path='/home' component={Home} />
-              <Route path='/questions/:id' component={QuestionPage} />
-              <Route exact path='/leaderboard' component={Leaderboard} />
-              <Route exact path='/add' component={NewQuestion} />
-            </div>
-        }
+        <Fragment>
+          <LoadingBar style={{ backgroundColor: '#00897B'}} />
+          {
+            this.props.loading
+              ? null
+              : <div className='app'>
+                <Route exact path='/' component={Login} />
+                <Route exact path='/home' component={Home} />
+                <Route path='/questions/:id' component={QuestionPage} />
+                <Route exact path='/leaderboard' component={Leaderboard} />
+                <Route exact path='/add' component={NewQuestion} />
+              </div>
+          }
+        </Fragment>
       </Router>
     );
   }
