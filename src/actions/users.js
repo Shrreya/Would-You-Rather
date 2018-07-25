a4d5e790a7531723e35051201bf1c0f481a4848b
shrreya <shrreyabhatachaarya@gmail.com>
2018-07-25 19:23:23 +0530

    Added action type to save new question for user

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const SAVE_USER_ANSWER = 'SAVE_USER_ANSWER';
+export const SAVE_USER_QUESTION = 'SAVE_USER_QUESTION';
 
 export function receiveUsers (users) {
   return {
@@ -14,5 +15,12 @@ export function userAnswer (info) {
     authedUser: info.authedUser,
     qid: info.qid,
     answer: info.answer
+  }
+}
+
+export function userQuestion (question) {
+  return {
+    type: SAVE_USER_QUESTION,
+    question
   }
 }
