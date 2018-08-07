b2a47b6627b59b3fc00804f1d91381c59b5401ca
shrreya <shrreyabhatachaarya@gmail.com>
2018-08-07 21:09:12 +0530

    Reusable option component

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -1,12 +1,10 @@
 // Packages
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Line } from 'rc-progress';
 // Components
 import Nav from './Nav';
 import LoginRedirect from './LoginRedirect';
-// Assets
-import check from '../assets/check.svg';
+import Option from './Option';
 // Action Creators
 import { handleAnswer } from '../actions/shared';
 // Utils
@@ -27,7 +25,7 @@ class QuestionPage extends Component {
     }
 
     const { question } = this.props;
-
+    // If question does not exist, display message
     if (question === null) {
       return (
         <div className='question-page'>
@@ -39,7 +37,6 @@ class QuestionPage extends Component {
 
     const { hasAnswered, authorName, authorAvatar, optionOne, optionTwo,
       answer, optionOneVotes, optionTwoVotes } = question;
-
     const totalVotes = optionOneVotes + optionTwoVotes;
     const optionOnePerc = getPercentVotes(optionOneVotes, totalVotes);
     const optionTwoPerc = getPercentVotes(optionTwoVotes, totalVotes);
@@ -50,40 +47,26 @@ class QuestionPage extends Component {
         <img className='big-avatar' alt='big avatar' src={require('../assets/' + authorAvatar)}/>
         <span className='author-prompt'>{authorName} asks</span>
         <h2>Would you rather ...</h2>
+
         {
-          hasAnswered && answer === 'optionOne' && <div>
-            <div className='option-close option-chosen'>
-              <div className='text-chosen'><strong>{optionOne}?</strong></div>
-              <Line percent={optionOnePerc} strokeColor='#00897B' className='percentage'/>
-              <span>{optionOnePerc}%</span>
-              <div>{optionOneVotes} out of {totalVotes} votes</div>
-              <img alt='badge' src={check} className='badge'/>
-            </div>
-            <div className='option-close'>
-              <div><strong>{optionTwo}?</strong></div>
-              <Line percent={optionTwoPerc} strokeColor='#00897B' className='percentage'/>
-              <span>{optionTwoPerc}%</span>
-              <div>{optionTwoVotes} out of {totalVotes} votes</div>
-            </div>
+          hasAnswered && <div>
+            <Option
+              chosen={answer === 'optionOne'}
+              optionText={optionOne}
+              optionPerc={optionOnePerc}
+              optionVotes={optionOneVotes}
+              totalVotes={totalVotes}
+            />
+            <Option
+              chosen={answer === 'optionTwo'}
+              optionText={optionTwo}
+              optionPerc={optionTwoPerc}
+              optionVotes={optionTwoVotes}
+              totalVotes={totalVotes}
+            />
           </div>
         }
-        {
-          hasAnswered && answer === 'optionTwo' && <div>
-            <div className='option-close'>
-              <div><strong>{optionOne}?</strong></div>
-              <Line percent={optionOnePerc} strokeColor='#00897B' className='percentage'/>
-              <span>{optionOnePerc}%</span>
-              <div>{optionOneVotes} out of {totalVotes} votes</div>
-            </div>
-            <div className='option-close option-chosen'>
-              <div className='text-chosen'><strong>{optionTwo}?</strong></div>
-              <Line percent={optionTwoPerc} strokeColor='#00897B' className='percentage'/>
-              <span>{optionTwoPerc}%</span>
-              <div>{optionTwoVotes} out of {totalVotes} votes</div>
-              <img alt='badge' src={check} className='badge'/>
-            </div>
-          </div>
-        }
+
         {
           !(hasAnswered) && <div>
             <div className='option-open' onClick={() => this.handleClick('optionOne')}>
